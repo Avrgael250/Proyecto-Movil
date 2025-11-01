eef25ee126cbef9e43a7a024d00e85cf15db1322
Rodrigo-Abed-Cruz-Diaz <124049938@upq.edu.mx>
2025-11-01 00:24:22 -0600

    Agregue los datos de ejemplo
    Agregue datos de ejemplo solo pare fines visuales durante la presentación del proyecto

---
+++ screens/DatosEjemplo.js
@@ -0,0 +1,38 @@
+export const transaccionesEjemplo = [
+    {
+        id:1,
+        tipo: 'ingreso',
+        monto : 20000,
+        categoria: 'Salario',
+        decripcion: 'Pago mensual de mi trabajo',
+        fecha: '2025-11-01',
+        fechaCreacion: new Date('2025-11-01')
+    },
+
+    {
+        id:2,
+        tipo: 'gasto',
+        monto : 750,
+        categoria: 'Comida',
+        decripcion: 'Gasto en supermercado',
+        fecha: '2025-11-02',
+        fechaCreacion: new Date('2025-11-02')
+    },
+
+    {
+        id:3,
+        tipo: 'gasto',
+        monto : 500,
+        categoria: 'Transporte',
+        decripcion: 'Gasto en gasolina del coche',
+        fecha: '2025-11-03',
+        fechaCreacion: new Date('2025-11-03')
+    },
+    
+];
+export const categoriasEjemplo = [
+    {id:1, mobre: 'Salario', tipo: 'ingreso', color: '#22C55E'},
+    {id:2, mobre: 'Comida', tipo: 'gasto', color: '#EF4444'},
+    {id:3, mobre: 'Transporte', tipo: 'gasto', color: '#F59E0B'},  
+    {id:4, mobre: 'Ocio', tipo: 'gasto', color: '#EC4899'},
+]        
